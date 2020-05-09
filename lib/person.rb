0770d54458743b9708d1955d1a4e13109d72628f
Shaina C <scailles23@gmail.com>
2020-05-09 09:02:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size
    :name => "Avi",
       :birthday => "01/29/1984",
       :hair_color => "brown",
