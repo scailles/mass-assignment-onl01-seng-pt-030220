90d9e5b104e6e92403bb2ec167dc5f6d1a7a4c9f
Shaina C <scailles23@gmail.com>
2020-05-09 09:01:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,20 @@
 class Person
   
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, 
+   :name => "Avi",
+      :birthday => "01/29/1984",
+      :hair_color => "brown",
+      :eye_color => "brown",
+      :height => "short",
+      :weight => "good",
+      :handed => "lefty",
+      :complexion => "decent",
+      :t_shirt_size => "medium",
+      :wrist_size => "small",
+      :glove_size => "normal",
+      :pant_length => "32",
+      :pant_width => "32"
+  
    def initialize(attributes)
     attributes.each do |key, value|
       self.send(("#{key}="), value)
