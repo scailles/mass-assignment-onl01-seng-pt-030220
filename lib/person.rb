8c360676e8bc6703b184ccb3c9fe2d1629112186
Shaina C <scailles23@gmail.com>
2020-05-09 09:05:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,8 @@
 class Person
   
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
    :name => "Avi",
-      :birthday => "01/29/1984",
-      :hair_color => "brown",
-      :eye_color => "brown",
-      :height => "short",
-      :weight => "good",
-      :handed => "lefty",
-      :complexion => "decent",
-      :t_shirt_size => "medium",
-      :wrist_size => "small",
-      :glove_size => "normal",
-      :pant_length => "32",
-      :pant_width => "32"
+      
   
    def initialize(attributes)
     attributes.each do |key, value|
